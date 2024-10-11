c0b24b6d93e2c2c4e11c73ffb14f883232491f1b
Carmen Chow <carmendevelops@gmail.com>
2024-10-11 00:29:29 -0400

    initial commit

---
+++ analysis.sql
@@ -0,0 +1,8 @@
+-- Histogram showing distribution of patients
+USE patient;
+SELECT ROUND(time_in_hospital, 1) AS bucket, 
+COUNT(*) AS count,
+RPAD('', COUNT(*)/100, '*') AS bar 
+FROM health
+GROUP BY bucket
+ORDER BY bucket;
