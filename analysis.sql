67874b7a9cb878e7b77c4a7630b75a2ddc253175
Carmen Chow <carmendevelops@gmail.com>
2024-10-14 19:37:23 -0400

    average stay in hospital

--- analysis.sql
+++ analysis.sql
@@ -1,4 +1,4 @@
--- Histogram showing distribution of patients
+-- Number of patients that spent # num of days in hospital
 SELECT 
 	time_in_hospital,
 	COUNT(*) AS count
@@ -6,5 +6,6 @@ FROM health
 GROUP BY time_in_hospital
 ORDER BY count desc
 
-
-
+-- Calculate average stay in hospital 
+SELECT AVG(CAST(time_in_hospital AS DECIMAL)) AS avg_time_in_hospital
+FROM health; -- average time 4.3960
