a29ef312a56f6f2ee22fc6a53be31a4d19e46f25
Carmen Chow <carmendevelops@gmail.com>
2024-10-14 20:41:29 -0400

    procedures by race

--- analysis.sql
+++ analysis.sql
@@ -9,3 +9,20 @@ ORDER BY count desc
 -- Calculate average stay in hospital 
 SELECT AVG(CAST(time_in_hospital AS DECIMAL)) AS avg_time_in_hospital
 FROM health; -- average time 4.3960
+
+-- Avg number of procedures per patient by specialty 
+SELECT medical_specialty,
+ROUND(AVG(num_procedures),1) as avg_procedures,
+COUNT(*) as num_of_patients
+FROM health
+GROUP BY medical_specialty
+HAVING num_of_patients > 50
+ORDER BY avg_procedures DESC;
+
+-- Procedures by race
+SELECT race,
+AVG(num_lab_procedures) as num_avg_procedures
+FROM health
+INNER JOIN demographics ON health.patient_nbr = demographics.patient_nbr
+GROUP BY race
+ORDER BY num_avg_procedures desc;
